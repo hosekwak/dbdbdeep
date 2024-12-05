5c1ca68cc80741ef81306220ca869781902af79e
hosekwak <podol2961@naver.com>
2024-12-05 16:19:01 +0900

    중간병합 확인 및 오류사항 수정

--- src/main/java/com/example/database_project/controller/HomeController.java
+++ src/main/java/com/example/database_project/controller/HomeController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home() {
         return "/home";
     }
+
+    @GetMapping("/home")
+    public String homepage() {
+        return "/home";
+    }
+
 }
